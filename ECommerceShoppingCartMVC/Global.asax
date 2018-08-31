8c3b795d4eae496457b1d1b9e3a9e32f13e98aae
Man Ioan <man_ioan@live.com>
2018-08-31 20:35:55 +0300

    Add project files.

---
+++ ECommerceShoppingCartMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ECommerceShoppingCartMVC.MvcApplication" Language="C#" %>
---
+++ ECommerceShoppingCartMVC/Models/Item.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ECommerceShoppingCartMVC.Models
+{
+    public class Item
+    {
+        private Produse produs = new Produse(); // Instantiate tblProduct class object 
+
+        #region Properties
+        public Produse Produs
+        {
+            get { return produs; }
+            set { produs = value; }
+        }
+        private int cantitate;
+
+        public int Cantitate
+        {
+            get { return cantitate; }
+            set { cantitate = value; }
+        }
+        #endregion
+
+        #region Constructors
+        // Default constructor
+        public Item()
+        {
+
+        }
+
+        // Parameterised constructor
+        public Item(Produse produs, int cantitate)
+        {
+            this.produs = produs;
+            this.cantitate = cantitate;
+        }
+        #endregion
+    }
+}
