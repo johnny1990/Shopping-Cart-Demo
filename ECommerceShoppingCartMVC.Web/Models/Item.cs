7c45f74ca2cf596c01e33435db4a89150d499f11
Man Ioan <man_ioan@live.com>
2020-10-04 09:01:21 +0300

    Add new application .NET Core

---
+++ ECommerceShoppingCartMVC.Web/Models/Item.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerceShoppingCartMVC.Web.Models
+{
+    public class Item
+    {
+        private Product prod = new Product();
+
+        #region Properties
+        public Product Product
+        {
+            get { return prod; }
+            set { prod = value; }
+        }
+        private int quantity;
+
+        public int Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+        #endregion
+
+        #region Constructors
+        public Item()
+        {
+
+        }
+
+        public Item(Product product, int qty)
+        {
+            this.prod = product;
+            this.quantity = qty;
+        }
+        #endregion
+    }
+}
---
+++ ECommerceShoppingCartMVC.Web/Models/ShoppingDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerceShoppingCartMVC.Web.Models
+{
+    public class ShoppingDBContext : DbContext
+    {
+
+        public ShoppingDBContext(DbContextOptions<ShoppingDBContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; }
+    }
+}
